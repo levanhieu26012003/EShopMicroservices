77ed4a9123b6e9a7226929f53571bf40c647f853
Le Hieu <levanhieu26012003@gmail.com>
2026-01-12 12:37:59 +0700

    Catalog init

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+});
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
